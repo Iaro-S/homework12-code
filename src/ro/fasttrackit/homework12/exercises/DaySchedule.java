8e3491d96c856012d4b69f1595df20bf7a637cae
Iaro <iaroslav.slovak@gmail.com>
2021-05-13 18:16:24 +0300

    Initial commit

--- src/ro/fasttrackit/homework12/exercises/DaySchedule.java
+++ src/ro/fasttrackit/homework12/exercises/DaySchedule.java
@@ -1,8 +1,6 @@
 package ro.fasttrackit.homework12.exercises;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class DaySchedule {
     private final Days days;
@@ -11,11 +9,5 @@ public class DaySchedule {
     public DaySchedule(Days days, List<String> activities) {
         this.days = days;
         this.activities = activities;
-    }
-
-    public Map<Days, List<String>> showActivities() {
-        Map<Days, List<String>> result = new HashMap<>();{
-        }
-        return result;
     }
 }
